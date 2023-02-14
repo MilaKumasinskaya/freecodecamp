6a2b5395a77d017235252e8dc60b05bb03a76f3d
MilaKumasinskaya <mila.kumasinskaya@mail.ru>
2023-02-14 19:19:38 +0300

    resolved task 1.76

---
+++ JS/1. Basic JavaScript/76. selecting_from_many_options_with_switch_statements.js
@@ -0,0 +1,25 @@
+function caseInSwitch(val) {
+    let answer = "";
+    switch(val) {// Only change code below this line
+        case 1:
+            answer = "alpha";
+            break;
+        case 2:
+            answer = "beta";
+            break;
+        case 3:
+            answer = "gamma";
+            break;
+        case 4:
+            answer = "delta";
+            break;
+    }
+
+    // Only change code above this line
+    return answer;
+}
+
+caseInSwitch(1);
+caseInSwitch(2);
+caseInSwitch(3);
+caseInSwitch(4);
