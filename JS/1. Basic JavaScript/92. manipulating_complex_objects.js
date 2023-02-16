2b2c9b0b025da6185424c3d630cfab30326e7d28
MilaKumasinskaya <mila.kumasinskaya@mail.ru>
2023-02-16 13:24:47 +0300

    resolved task 1.92

---
+++ JS/1. Basic JavaScript/92. manipulating_complex_objects.js
@@ -0,0 +1,25 @@
+const myMusic = [
+    {
+        "artist": "Billy Joel",
+        "title": "Piano Man",
+        "release_year": 1973,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    },
+    {
+        "artist": "MMM",
+        "title": "Man",
+        "release_year": 2023,
+        "formats": [
+            "CD",
+            "8T",
+            "LP"
+        ],
+        "gold": true
+    }
+
+];
